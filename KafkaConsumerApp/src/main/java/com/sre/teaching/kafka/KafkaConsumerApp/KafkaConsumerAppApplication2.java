fb5266f13db07e56eea12682c510cdfd43a15370
sreedharsingaraju <38294585+sreedharsingaraju@users.noreply.github.com>
2023-02-22 15:26:52 +0530

    Kafka Consuming Application
    Consumer Application for receiving messages from Kafka

---
+++ KafkaConsumerApp/src/main/java/com/sre/teaching/kafka/KafkaConsumerApp/KafkaConsumerAppApplication2.java
@@ -0,0 +1,33 @@
+package com.sre.teaching.kafka.KafkaConsumerApp;
+
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class KafkaConsumerAppApplication2 {
+
+	static Map<String,Object> Configure()
+	{
+		Map<String,Object> configuration=new HashMap<>();
+
+		configuration.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092, localhost:9093, localhost:9094");
+		configuration.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		configuration.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class.getName());
+		configuration.put(ConsumerConfig.GROUP_ID_CONFIG,"Consumer.3");
+		//configuration.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest");
+
+		return  configuration;
+	}
+	public static void main(String[] args) {
+
+		MyCustomConsumer kafkaConsumeMessages=new MyCustomConsumer(Configure());
+
+		System.out.println("Awaiting Messages ....");
+		kafkaConsumeMessages.PollKafka();
+
+	}
+
+}
